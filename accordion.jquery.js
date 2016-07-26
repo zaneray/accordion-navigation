a68577f8ad0f0dfa039b2aeb6878cc91a05ac8d9
sebbyt16 <thomas.sebby@zaneray.com>
2016-07-26 10:18:25 -0600

    *Clean code, add all names, classes, speeds, etc as options and not hard coded * Add multi-open true/false

--- accordion.jquery.js
+++ accordion.jquery.js
@@ -2,22 +2,28 @@
 // Mobile Accordion expand/collaspe toggle and trigger active class to update UI icons
 // Can pass in options to get specific selectors, and optional callback function
 (function ($) {
-    $.fn.mobileAccordion = function (args) {
+    $.fn.simpleAccordion = function (args) {
         var options = {
-            selector: '.js-mobile-accordion-header',
-            contentSelector: '.js-mobile-accordion-content',
+            selector: '.js-accordion-header',
+            contentSelector: '.js-accordion-content',
+            activeClassSelector: 'active',
+            multipleOpen: true,
+            animationSpeed: 250,
             callback: function () { }
         };
-
         $.extend(options, args);
 
         return this.each(function () {
             var $parentSelector = $(this);
 
             $parentSelector.on('click', options.selector, function (e) {
+                var $this = $(this);
                 e.stopImmediatePropagation();
                 e.preventDefault();
-                $(this).toggleClass('active').next(options.contentSelector).slideToggle(250, function () {
+                if (options.multipleOpen == false && !$this.hasClass(options.activeClassSelector)) {
+                    $parentSelector.find("." + options.activeClassSelector).toggleClass(options.activeClassSelector).next(options.contentSelector).slideToggle();
+                }
+                $this.toggleClass(options.activeClassSelector).next(options.contentSelector).slideToggle(options.animationSpeed, function () {
                     options.callback.call(this);
                 });
             });
